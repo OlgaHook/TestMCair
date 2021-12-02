68cc1c1cae3d0747cdfe2e7d4d545e2801a1ad99
Olga Hook <olga_krjuckova@inbox.lv>
2021-12-02 09:15:32 +0200

    MacAirFirstConnectionTest

---
+++ testMCAir/testMCAir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testMCAir
+//
+//  Created by Olga Krjuckova on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
