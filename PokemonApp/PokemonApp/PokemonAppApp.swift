2b6027806ea52fe7efbe4c2512c49bd6a68e50a5
Olga Hook <olga_krjuckova@inbox.lv>
2022-03-02 09:36:57 +0200

    002.03.2022

---
+++ PokemonApp/PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Olga Krjuckova on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
